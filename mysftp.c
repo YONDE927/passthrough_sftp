b6cef04d9e26e8216cf70a16aab3df94a93ae93d
Yuta <svyuta1202@gmail.com>
2021-10-01 03:02:02 +0900

    lib link

--- mysftp.c
+++ mysftp.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <libssh2.h>
-#include <libssh2_config.h>
+#include "libssh2_config.h"
 #include <libssh2_sftp.h>
 #include <WinSock2.h>
+#pragma comment(lib, "Ws2_32.lib")
 
-const char* keyfile = "C:/Users/svyut/.ssh/redmine.pem";
-const char* username = "redmine_user";
-const char* server_ip = "153.126.189.240";
-
+//const char* keyfile = "C:/Users/svyut/.ssh/redmine.pem";
+const char* username = "yuta";
+const char* server_ip = "172.25.217.120";
+const char* password = "taiki927";
+const char* sftppath = "/home/yuta/test.txt";
 int main(int argc, char* argv[]){
 	unsigned long hostaddr;
 	int sock,i,auth_pw=0;
@@ -42,7 +44,7 @@ int main(int argc, char* argv[]){
 	sin.sin_port = htons(22);
 	sin.sin_addr.s_addr = hostaddr;
 	if(connect(sock,(struct sockaddr*)&sin,sizeof(struct sockaddr_in))!=0){
-		fprintf(stderr,"connection failed %d.\n");
+		fprintf(stderr,"connection failed .\n");
 		return -1;
 	}
 
@@ -57,5 +59,50 @@ int main(int argc, char* argv[]){
 
 	rc = libssh2_session_handshake(session,sock);
 
+	fingerprint = libssh2_hostkey_hash(session,LIBSSH2_HOSTKEY_HASH_SHA1);
+
+	userauthlist = libssh2_userauth_list(session,username,strlen(username));
+
+	if(libssh2_userauth_password(session,username,password)){
+		fprintf(stderr,"Auth by password failed.\n");
+		goto shutdown;
+	}
+
+	//sftp session
+	fprintf(stderr,"libssh2_sftp_init()!\n");
+	sftp_session = libssh2_sftp_init(session);
+	
+	//sftp open
+	sftp_handle = libssh2_sftp_open(sftp_session,sftppath,LIBSSH2_FXF_READ,0);
+	if(!sftp_handle){
+		fprintf(stderr,"Unable to open file\n");
+		goto shutdown;
+	}
+	fprintf(stderr,"sftp_open is done\n");
+
+	do{
+		char mem[1024];
+		rc = libssh2_sftp_read(sftp_handle,mem,sizeof(mem));
+		if(rc>0){
+			fwrite(mem,rc,rc,stdout);
+		}else{
+			break;
+		}
+	}while(1);
+
+	//sftp close
+	libssh2_sftp_close(sftp_handle);
+	//sftp session shutdown
+	libssh2_sftp_shutdown(sftp_session);
+
+
+shutdown:
+	libssh2_session_disconnect(session,"Normal Shutdown");
+	libssh2_session_free(session);
+	closesocket(sock);
+	libssh2_exit();
 	return 0;
+
+
+
 }
