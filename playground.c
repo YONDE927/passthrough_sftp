53d2342cd9611925788a5401514838e9a2b2dfab
YONDE927 <svyuta1202@gmail.com>
2021-10-08 05:33:07 +0900

    a

--- playground.c
+++ playground.c
@@ -2,23 +2,21 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define REMOTEPOINT "/home/yuta/tmp"
 
 int main(){
-    int fd,nwritten,nbytes;
-    char* buffer = "abcdefg";
-    fd = open("I:\\DOC\\passthrough_sftp\\test.txt", O_CREAT|O_WRONLY,0777);
-    if (fd < 0) {
-      fprintf(stderr, "Can't open file for writing: %s\n",
-              strerror(errno));
-      return -1;
-    }
-    nwritten = write(fd, buffer, sizeof(buffer)-1);
-    //printf("%d\n",sizeof(buffer)-1);
-    //printf("%d\n",nwritten);
-    if (nwritten != (sizeof(buffer)-1)) {
-        fprintf(stderr, "Error writing: %s\n",
-                strerror(errno));
-        return -1;
-    }
+    char *remote_path;
+    char *path = "/test.txt";
+    int len;
+    len = strlen(REMOTEPOINT);
+    printf("%d\n",len);
 
+    remote_path = (char*)malloc(strlen(REMOTEPOINT) + strlen(path));
+    strcpy(remote_path,REMOTEPOINT);
+    strcat(remote_path,path);
+    printf("%s\n",remote_path);
+
+    return 0;
 }
